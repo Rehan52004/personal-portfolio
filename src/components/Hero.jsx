c36a29ff1165f0bfcee418fd8f685deb8a311b4a
Rehan <rehancodes786@gmail.com>
2024-06-02 00:12:46 +0530

    new projects added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,85 +2,86 @@ import React from 'react';
 import { Col, Container, NavLink, Row } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 
-import resume from '../assets/rehan-resume.pdf';
+import resume from '../assets/new-resume.pdf';
 const Hero = () => {
-	return (
-		<>
-			<div className="hero h-100" id="home">
-				<Container className="mb-3">
-					<Row className="justify-content-center my-1 my-md-5 py-4 py-md-5">
-						<Col
-							lg={10}
-							style={{
-								display: 'flex',
-								alignItems: 'center',
-							}}
-						>
-							<div className="left-text text-center">
-								<motion.div
-									initial={{ x: -1000 }}
-									animate={{ x: 0 }}
-									transition={{ delay: 0.5 }}
-								>
-									<h1 style={{ fontWeight: 500 }}>Hello, I am</h1>
-								</motion.div>
-								<motion.div
-									initial={{ x: -1500 }}
-									animate={{ x: 0 }}
-									transition={{ delay: 1 }}
-								>
-									<p>Abdul Rehan</p>
-								</motion.div>
-								<motion.div
-									initial={{ opacity: 0, scale: 0.5 }}
-									animate={{ opacity: 1, scale: 1 }}
-									transition={{
-										duration: 1,
-										delay: 1.2,
-										ease: [0, 0.71, 0.2, 1.01],
-									}}
-								>
-									<h4>
-										Rehan, a 3rd-year BCA student specializing in
-										computer science, is proficient in frontend
-										development, blending creativity and code to
-										deliver exceptional user experiences.
-									</h4>
-								</motion.div>
-								<motion.div
-									className="btns"
-									initial={{ opacity: 0, scale: 0.5 }}
-									animate={{ opacity: 1, scale: 1 }}
-									transition={{
-										duration: 1,
-										delay: 1.6,
-										ease: [0, 0.71, 0.2, 1.01],
-									}}
-								>
-									<button className="type-1-btn me-3">
-										<NavLink href="#contactme">Contact Me</NavLink>
-									</button>
-									<button className="type-2-btn">
-										<a
-											href={resume}
-											download
-											style={{
-												color: 'black',
-												textDecoration: 'none',
-											}}
-										>
-											Download CV
-										</a>
-									</button>
-								</motion.div>
-							</div>
-						</Col>
-					</Row>
-				</Container>
-				<hr color="black" />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className='hero h-100' id='home'>
+        <Container className='mb-3'>
+          <Row className='justify-content-center my-1 my-md-5 py-4 py-md-5'>
+            <Col
+              lg={10}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <div className='left-text text-center'>
+                <motion.div
+                  initial={{ x: -1000 }}
+                  animate={{ x: 0 }}
+                  transition={{ delay: 0.5 }}
+                >
+                  <h1 style={{ fontWeight: 500 }}>Hello, It's me</h1>
+                </motion.div>
+                <motion.div
+                  initial={{ x: -1500 }}
+                  animate={{ x: 0 }}
+                  transition={{ delay: 1 }}
+                >
+                  <p>Rehan.</p>
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    duration: 1,
+                    delay: 1.2,
+                    ease: [0, 0.71, 0.2, 1.01],
+                  }}
+                >
+                  <h5 style={{ marginBottom: '15px' }}>
+                    Hi, I'm Rehan, a final-year BCA student about to graduate. I
+                    have experience in building front-end web applications with
+                    React and am passionate about creating responsive user
+                    interfaces. I am open to work opportunities to apply my
+                    skills and contribute to exciting projects.
+                  </h5>
+                </motion.div>
+                <motion.div
+                  className='btns'
+                  initial={{ opacity: 0, scale: 0.5 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  transition={{
+                    duration: 1,
+                    delay: 1.6,
+                    ease: [0, 0.71, 0.2, 1.01],
+                  }}
+                >
+                  <button className='type-1-btn me-3'>
+                    <NavLink href='#contactme'>Contact Me</NavLink>
+                  </button>
+                  <button className='type-2-btn'>
+                    <a
+                      href={resume}
+                      download
+                      style={{
+                        color: 'black',
+                        textDecoration: 'none',
+                      }}
+                    >
+                      Download CV
+                    </a>
+                  </button>
+                </motion.div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <hr color='black' />
+      </div>
+    </>
+  );
 };
 
 export default Hero;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,52 +8,64 @@ import project3 from '../assets/images/project-3.png';
 import project4 from '../assets/images/project-4.png';
 import project5 from '../assets/images/project-5.png';
 import project6 from '../assets/images/project-6.png';
+import project7 from '../assets/images/project-7.png';
+import project8 from '../assets/images/project-8.png';
 
 //components
 import ProjectCard from './ProjectCard';
 
 const Projects = () => {
-	return (
-		<>
-			<div className="projects" id="projects">
-				<h1 className="text-center">Projects</h1>
-				<Container className="my-5">
-					<Row className="row-gap-5 my-5">
-						<ProjectCard
-							code="https://github.com/Rehan52004/To-Do-List"
-							live="https://to-do-list-website1.netlify.app/"
-							image={project1}
-						/>
-						<ProjectCard
-							code="https://github.com/Rehan52004/google-cloud-clone"
-							live="https://google-cloud-clone.netlify.app/"
-							image={project2}
-						/>
-						<ProjectCard
-							code="https://github.com/Rehan52004/Barber-Website"
-							live="https://rehan52004.github.io/Barber-Website/"
-							image={project3}
-						/>
-						<ProjectCard
-							code="https://github.com/Rehan52004/farhan-graphics"
-							live="https://farhangraphics.netlify.app"
-							image={project4}
-						/>
-						<ProjectCard
-							code="https://github.com/Rehan52004/Businesss-Web-App"
-							live="https://rehan52004.github.io/Businesss-Web-App/"
-							image={project5}
-						/>
-						<ProjectCard
-							code="https://github.com/Rehan52004/Movie-hub"
-							live="https://github.com/Rehan52004/Movie-hub"
-							image={project6}
-						/>
-					</Row>
-				</Container>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className='projects' id='projects'>
+        <h1 className='text-center'>Projects</h1>
+        <Container className='my-5'>
+          <Row className='row-gap-5 my-5'>
+            <ProjectCard
+              code='https://github.com/Rehan52004/To-Do-List'
+              live='https://to-do-list-website1.netlify.app/'
+              image={project1}
+            />
+            <ProjectCard
+              code='https://github.com/Rehan52004/google-cloud-clone'
+              live='https://google-cloud-clone.netlify.app/'
+              image={project2}
+            />
+            <ProjectCard
+              code='https://github.com/Rehan52004/Barber-Website'
+              live='https://rehan52004.github.io/Barber-Website/'
+              image={project3}
+            />
+            <ProjectCard
+              code='https://github.com/Rehan52004/farhan-graphics'
+              live='https://farhangraphics.netlify.app'
+              image={project4}
+            />
+            <ProjectCard
+              code='https://github.com/Rehan52004/Businesss-Web-App'
+              live='https://rehan52004.github.io/Businesss-Web-App/'
+              image={project5}
+            />
+            <ProjectCard
+              code='https://github.com/Rehan52004/Movie-hub'
+              live='https://github.com/Rehan52004/Movie-hub'
+              image={project6}
+            />
+            <ProjectCard
+              code='https://github.com/Rehan52004/firebase-blog'
+              live='https://blog--sphere.vercel.app'
+              image={project7}
+            />
+            <ProjectCard
+              code='https://github.com/Rehan52004/design-cargo'
+              live='https://github.com/Rehan52004/design-cargo'
+              image={project8}
+            />
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default Projects;
